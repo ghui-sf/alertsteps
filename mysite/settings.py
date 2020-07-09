6d43a75ac0bc3c7a725979bf05adfbed8d80faf8
Gary Hui <gary.hui@salesforce.com>
2020-07-09 18:47:30 -0400

    add 'alertsteps-stag.herokuapp.com' to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zv*_oha868ozpqe6i5+h$ar5!i^gn)w__kfsq&2azav4s%jwrn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '0.0.0.0', 'alertsteps-stag.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [ 'alertsteps-stag.herokuapp.com' , '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
